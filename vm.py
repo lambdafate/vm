df4eddc26be4cf703a6e857d1a5f0c2fd5ad255b
lambdafate <lambdafate@163.com>
2020-02-25 17:50:10 +0800

    little virtual machine

--- vm.py
+++ vm.py
@@ -1,12 +1,5 @@
 import dis
 
-source = """
-a = 3
-b = 2
-print("Hello", "asdf", a+b)
-"""
-
-
 class VM(object):
     def __init__(self, codeinfo):
         self.consts = codeinfo.get("consts", ())
@@ -16,84 +9,93 @@ class VM(object):
         self.envs = {}
         self.stack = []
 
+        self._in_consts = ["LOAD_CONST"]
+        self._in_names = ["STORE_NAME", "LOAD_NAME"]
+        self._return_arg = ["CALL_FUNCTION"]
+
+
+    def push(self, arg):
+        self.stack.append(arg)
+
+    def pop(self):
+        if self.stack:
+            return self.stack.pop()
+        return None
+
     # ins: (insname, oparg)
     def getargs(self, ins):
-        codenum = dis.opmap.get(ins[0], -1)
-        if codenum < 90:
+        if ins.opcode < 90:
             return None
-        if ins[0] in ["LOAD_CONST"]:
-            return self.consts[ins[1]]
-        elif ins[0] in ["STORE_NAME", "LOAD_NAME"]:
-            return self.names[ins[1]]
-        elif ins[0] in ["CALL_FUNCTION"]:
-            return ins[1]
+        if ins.opname in self._in_consts:
+            return self.consts[ins.arg]
+        elif ins.opname in self._in_names:
+            return self.names[ins.arg]
+        elif ins.opname in self._return_arg:
+            return ins.arg
         return None
         
 
     def run(self):
         for ins in self.instructions:
-            if not hasattr(self, ins[0].lower()):
-                raise Exception(f"not found function: {ins[0]}")
+            bytecode = ins.opname.lower()
+            if not hasattr(self, bytecode):
+                raise Exception(f"not found function: {bytecode}")
                 
-            func = getattr(self, ins[0].lower())
+            func = getattr(self, bytecode)
             # 在每条指令执行之前, 解析出指令参数对应的真实值
-            if dis.opmap.get(ins[0], -1) < 90:
-                func()
-            else:
+            if ins.opcode >= 90:
                 arg = self.getargs(ins)
                 func(arg)
+            else:
+                func()
 
+
+### 以下为所有已经实现的字节码
     def pop_top(self):
-        self.stack.pop()
+        self.pop()
 
     def binary_add(self):
-        arg1 = self.stack.pop()
-        arg2 = self.stack.pop()
-        self.stack.append(arg1+arg2)
+        arg1 = self.pop()
+        arg2 = self.pop()
+        self.push(arg1+arg2)
 
     def return_value(self):
-        self.stack.pop()
+        self.pop()
 
     def load_const(self, arg):
-        self.stack.append(arg)
+        self.push(arg)
     
     def store_name(self, arg):
-        self.envs[arg] = self.stack.pop()
+        self.envs[arg] = self.pop()
 
     def load_name(self, arg):
-        self.stack.append(self.envs.get(arg, arg))
+        self.push(self.envs.get(arg, arg))
 
     def call_function(self, arg):
         args = []
         for _ in range(arg):
-            args.insert(0, self.stack.pop())
-        func = self.stack.pop()
+            args.insert(0, self.pop())
+        func = self.pop()
         if func == "print":
             for arg in args:
                 print(arg, end="")
-        self.stack.append(None)
+        self.push(None)
 
 
 
-def parser(source):
-    bytecode = dis.Bytecode(source)
-    instructions = [(ins.opname, ins.arg) for ins in bytecode]
-
-    codeobject = bytecode.codeobj
-    # 获取常量表和变量名表
-    consts = codeobject.co_consts
-    names = codeobject.co_names
-
-    codeinfo = {
-        "consts": consts,
-        "names": names,
-        "instructions": instructions
-    }
-
-    return codeinfo
 
 
-codeinfo = parser(source)
-print(codeinfo)
-vm = VM(codeinfo)
-vm.run()
+if __name__ == "__main__":
+    from convert import parser
+
+    source = """
+a = 1 
+b = 2
+c = a+b
+print(c)
+    """
+    
+    codeinfo = parser(source)
+    # print(codeinfo)
+    vm = VM(codeinfo)
+    vm.run()
